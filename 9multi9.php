eea3b2446cfc9a4f2e2e94e23e54569f4e60b7c3
Yemonth <qsc995860@gmail.com>
2020-12-09 07:36:58 +0800

    Add files via upload

---
+++ 9multi9.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>p10-2</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<?PHP 
+	for($a=1;$a<10;$a++){
+		for($b=1;$b<10;$b++){
+			echo $a+"x"+$b+"="$a*$b;
+			echo " ";
+		}
+		echo "\n";
+	}
+?>
+</body>
+</html>
