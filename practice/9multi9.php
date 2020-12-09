87a67808d6a08356849b9504f007f12ae73c8301
Yemonth <qsc995860@gmail.com>
2020-12-09 10:36:33 +0800

    Update 9multi9.php

--- practice/9multi9.php
+++ practice/9multi9.php
@@ -7,8 +7,9 @@
 <?PHP 
 	for($a=1;$a<10;$a++){
 		for($b=1;$b<10;$b++){
+			echo'<div style="display:inline-block; width:50px;margin-left:10px;">';
 			echo $a."x".$b."=".$a*$b;
-			echo " ";
+			echo "</div>";
 		}
 		echo "\n";
 	}
